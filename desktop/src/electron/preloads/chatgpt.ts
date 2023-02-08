1e19cdd565c8f7530016467c93bfa53d0331bb30
holma91 <alexanderholmberg91@gmail.com>
2023-02-08 18:06:10 +0100

    add chatgpt

--- desktop/src/electron/preloads/chatgpt.ts
+++ desktop/src/electron/preloads/chatgpt.ts
@@ -9,9 +9,6 @@ contextBridge.exposeInMainWorld('api', relevantApi);
 
 const css = `
   /* input */
-  .m-0.w-full.resize-none.border-0.bg-transparent.p-0.pl-2.pr-7.focus:ring-0.focus-visible:ring-0.dark:bg-transparent.md:pl-0 {
-    background-color: green;
-  }
 
   body::-webkit-scrollbar {
     display: none;
@@ -19,3 +16,21 @@ const css = `
 `;
 
 webFrame.insertCSS(css);
+
+window.addEventListener('DOMContentLoaded', () => {
+  const searchInput: any = document.querySelector(
+    '.m-0.w-full.resize-none.border-0.bg-transparent.p-0.pl-2.pr-7'
+  );
+  const searchButton: any = document.querySelector(
+    '.absolute.p-1.rounded-md.text-gray-500'
+  );
+
+  ipcRenderer.on('searchInput', (_event, value) => {
+    if (value === 'Enter') {
+      // search
+      searchButton.click();
+    } else {
+      searchInput.value = value;
+    }
+  });
+});
